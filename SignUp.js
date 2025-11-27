17d478091f6dc151e1ec692e6d25be0de0730c02
rzalejandro6 <rjisaacalejandro18@gmail.com>
2025-11-27 21:50:55 +0800

    last checked - Nov 27, 2025 - 9:50pm

--- SignUp.js
+++ SignUp.js
@@ -1,20 +1,22 @@
-// Select the signup form
+
 const form = document.getElementById('signupForm');
 
-// Listen for form submission
-form.addEventListener('submit', function(e) {
-    e.preventDefault(); // Prevent default form submission
 
-    // Get input values
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+
+    
     const email = document.getElementById('email').value;
     const password = document.getElementById('password-input').value;
 
-    // Check if both fields are filled
+
     if (email && password) {
         alert('You have created an account!');
-        window.location.href = "LogIn.html"; // Redirect to login page
+        window.location.href = "LogIn.html";
     } else {
         alert('Please fill out all fields.');
     }
 });
+
 
